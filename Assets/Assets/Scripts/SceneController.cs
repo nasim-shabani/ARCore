e854768aebfa20a462cd20a646377683ab5ea3fd
Nasim Shabani <Nasim.ShabaniLalani@student.pxl.be>
2018-08-21 01:11:49 +0200

    Uploaded!

---
+++ Assets/Assets/Scripts/SceneController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+
+public class SceneController : MonoBehaviour {
+
+    public Camera firstPersonCamera;
+    public ScoreboardController scoreboard;
+    public SnakeController snakeController;
+
+    void Start()
+    {
+        QuitOnConnectionErrors();
+    }
+
+    void QuitOnConnectionErrors()
+    {
+        if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
+        {
+            StartCoroutine(ToastMsgUtils.ToastAndExit(
+                "Camera permission is needed to run this application.", 5));
+        }
+        else if (Session.Status.IsError())
+        {
+            // This covers a variety of errors.  See reference for details
+            // https://developers.google.com/ar/reference/unity/namespace/GoogleARCore
+            StartCoroutine(ToastMsgUtils.ToastAndExit(
+                "ARCore encountered a problem connecting. Please restart the app.", 5));
+        }
+    }
+
+    void Update()
+    {
+        // The session status must be Tracking in order to access the Frame.
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            int lostTrackingSleepTimeout = 15;
+            Screen.sleepTimeout = lostTrackingSleepTimeout;
+            return;
+        }
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
+        ProcessTouches();
+
+        scoreboard.SetScore(snakeController.GetLength());
+
+    }
+
+    void ProcessTouches()
+    {
+        Touch touch;
+        if (Input.touchCount != 1 ||
+            (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            return;
+        }
+
+        TrackableHit hit;
+        TrackableHitFlags raycastFilter =
+            TrackableHitFlags.PlaneWithinBounds |
+            TrackableHitFlags.PlaneWithinPolygon;
+
+        if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit))
+        {
+            SetSelectedPlane(hit.Trackable as DetectedPlane);
+        }
+    }
+
+    void SetSelectedPlane(DetectedPlane selectedPlane)
+    {
+        Debug.Log("Selected plane centered at " + selectedPlane.CenterPose.position);
+        snakeController.SetPlane(selectedPlane);
+        scoreboard.SetSelectedPlane(selectedPlane);
+        GetComponent<FoodController>().SetSelectedPlane(selectedPlane);
+    }
+
+}
